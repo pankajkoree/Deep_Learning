e37188a71a450b72b91e923c7f2e24ccc00ff62e
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-09-04 10:13:24 +0530

    Add files via upload

---
+++ Day179 Deep Learning Day 9/vanishing_gradient_problem.py
@@ -0,0 +1,61 @@
+# using relu
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from keras.layers import Dense
+from keras.models import Sequential
+
+
+X,y = make_moons(n_samples=250, noise=0.05, random_state=42)
+
+plt.scatter(X[:,0],X[:,1], c=y, s=100)
+plt.show()
+
+model = Sequential()
+
+model.add(Dense(10,activation='relu',input_dim=2))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(10,activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+
+print(model.get_weights()[0])
+
+print(model.get_weights()[0])
+
+
+old_weights = model.get_weights()[0]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+model.fit(X_train, y_train, epochs = 1)
+
+new_weights = model.get_weights()[0]
+
+print(model.optimizer.get_config()["learning_rate"])
+
+gradient = (old_weights - new_weights)/ 0.001
+percent_change = abs(100*(old_weights - new_weights)/ old_weights)
+
+
+print(gradient)
+
+print(percent_change)
+
+print(old_weights)
+
+print(new_weights)
