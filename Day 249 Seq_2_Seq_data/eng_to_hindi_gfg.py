064c78edee8f51ba1b491b74b3d97f97aeb6e7ad
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-12-27 15:45:05 +0530

    Add files via upload

---
+++ Day 249 Seq_2_Seq_data/eng_to_hindi_gfg.py
@@ -0,0 +1,11 @@
+# importing the module
+from englistohindi import EngtoHindi
+
+# message to be translated
+message = "Yes, I am geeks"
+ 
+# creating a EngtoHindi() object
+res = EngtoHindi(message)
+ 
+# displaying the translation
+print(res.convert)
